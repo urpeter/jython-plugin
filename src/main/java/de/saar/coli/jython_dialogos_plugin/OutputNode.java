44071c09041c6b79ef728a79583aeee1073b808d
Alexander Koller <koller@ling.uni-potsdam.de>
2018-10-03 10:34:01 +0200

    minimal java plugin

---
+++ src/main/java/de/saar/coli/jython_dialogos_plugin/OutputNode.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.saar.coli.jython_dialogos_plugin;
+
+import com.clt.diamant.graph.nodes.AbstractOutputNode;
+import com.clt.diamant.graph.nodes.AbstractOutputNode.DefaultPromptType;
+import com.clt.speech.SpeechException;
+import com.clt.speech.tts.VoiceName;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author koller
+ */
+public class OutputNode extends AbstractOutputNode {
+
+    @Override
+    protected IPromptType getDefaultPromptType() {
+        return DefaultPromptType.text;
+    }
+
+    @Override
+    public String getResourceString(String string) {
+        return string;
+    }
+
+    @Override
+    protected List<VoiceName> getAvailableVoices() {
+        return Collections.singletonList(new VoiceName("", null));
+    }
+
+    @Override
+    protected void speak(String string, Map<String, Object> map) throws SpeechException {
+        System.err.println("speak: " + string);
+        System.err.println(" -> with map: " + map);
+    }
+
+    @Override
+    protected void stopSynthesis() {
+    }
+    
+}
